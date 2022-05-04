48fa046747eb1ef94a722359921c9b59fffb5035
Stephen McDonald <steve@jupo.org>
2022-05-05 09:16:15 +1000

    start

--- app.js
+++ app.js
@@ -20,50 +20,12 @@ const bodyParser = require('body-parser');
 const { GoogleAuth } = require('google-auth-library');
 const jwt = require('jsonwebtoken');
 
-const serviceAccountFile = process.env.GOOGLE_APPLICATION_CREDENTIALS || '/path/to/key.json';
-const issuerId = process.env.WALLET_ISSUER_ID || '<issuer ID>';
-const classId = process.env.WALLET_CLASS_ID || 'test-class-id';
+const serviceAccountFile = '/path/to/key.json';
+const issuerId = '';
+const classId = '';
 
 async function createPassAndToken(req, res) {
-  const credentials = require(serviceAccountFile);
-  const httpClient = new GoogleAuth({
-    credentials: credentials,
-    scopes: 'https://www.googleapis.com/auth/wallet_object.issuer'
-  });
-
-  const objectUrl = 'https://walletobjects.googleapis.com/walletobjects/v1/genericObject/';
-  const objectPayload = require('./generic-pass.json');
-
-  objectPayload.id = `${issuerId}.${req.body.email.replace(/[^\w.-]/g, '_')}-${classId}`;
-  objectPayload.classId = `${issuerId}.${classId}`;
-
-  let objectResponse;
-  try {
-    objectResponse = await httpClient.request({url: objectUrl + objectPayload.id, method: 'GET'});
-    console.log('existing object', objectPayload.id);
-  } catch (err) {
-    if (err.response && err.response.status === 404) {
-      objectResponse = await httpClient.request({url: objectUrl, method: 'POST', data: objectPayload});
-      console.log('new object', objectPayload.id);
-    } else {
-      console.error(err);
-      throw err;
-    }
-  }
-
-  const claims = {
-    iss: credentials.client_email, // `client_email` in service account file.
-    aud: 'google',
-    origins: ['http://localhost:3000'],
-    typ: 'savetowallet',
-    payload: {
-      genericObjects: [{id: objectPayload.id}],
-    },
-  };
-
-  const token = jwt.sign(claims, credentials.private_key, {algorithm: 'RS256'});
-  const saveUrl = `https://pay.google.com/gp/v/save/${token}`;
-  res.send(`<a href="${saveUrl}"><img src="wallet-button.png"></a>`);
+  res.send("Form submitted!");
 }
 
 const app = express();
