3f535e448a26efc0050ca0373a814fd60b1139ba
Jose Ugia <ugia@google.com>
2025-01-07 13:22:04 +0100

    Remove unused require

--- web_complete/app.js
+++ web_complete/app.js
@@ -15,7 +15,6 @@
  */
 
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const { GoogleAuth } = require('google-auth-library');
 const jwt = require('jsonwebtoken');
